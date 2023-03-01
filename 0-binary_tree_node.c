a326f5bb7baccce6434a96d2b6f622249096c7d0
Iyanu456 <oyerindei13@gmail.com>
2023-03-02 00:48:09 +0100

    default message

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,4 +9,13 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *node;
+
+	node = malloc(sizeof(binary_tree_t));
+	node.n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+
+	return(node);
 }

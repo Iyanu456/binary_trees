b0c24aa70610f63d50ad1c4cd1535f4b01e0f01d
Iyanu456 <oyerindei13@gmail.com>
2023-03-02 00:54:14 +0100

    default message

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = malloc(sizeof(binary_tree_t));
-	node.n = value;
+	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;

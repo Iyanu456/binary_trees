6e70696157623e88171b2a92a14b876ffed6abbf
Iyanu456 <oyerindei13@gmail.com>
2023-03-02 12:59:43 +0100

    default message

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - checks if a node is a root node
+ * @node: node
+ * Description: checks if a node is a root node
+ * Return: int
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
+}

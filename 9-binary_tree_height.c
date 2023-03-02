20ebec32cf20211b1b0da79bb60c2a2144c8eb42
Iyanu456 <oyerindei13@gmail.com>
2023-03-02 14:31:04 +0100

    default message

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - calculates the height of a tree
+ * @tree: binary tree
+ * Description: calculates the height of a tree
+ * Return: size_t
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+	return (1 + (left_height > right_height ? left_height : right_height));
+}

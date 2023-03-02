85dd938bd7b892be6dead192bf9d8f07456d6b1a
Iyanu456 <oyerindei13@gmail.com>
2023-03-02 01:23:13 +0100

    default message

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,7 +14,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 		return (NULL);
-	
+
+	parent->left = node;
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
@@ -23,6 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)
 	{
 		node->left = parent->left;
+		parent->left->parent = node;
 		parent->left = node;
 	}
 	return (node);

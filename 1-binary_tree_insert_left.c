63a321b17f982ea5f7373e646e0af419c5e2739a
Iyanu456 <oyerindei13@gmail.com>
2023-03-02 08:49:56 +0100

    default message

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_insert_left - inserts a node as the left child of another node
  * @value: integer
@@ -11,7 +12,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	node = malloc(sizeof(binary_tree_t));
+	node = calloc(1, sizeof(binary_tree_t));
 	if (node == NULL)
 		return (NULL);
 
